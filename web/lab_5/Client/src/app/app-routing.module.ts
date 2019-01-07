0aea04ba0bb5ef6a12ffdbb2e763f5c848f0ade3
zoOm60rus <xam9lk60rus@gmail.com>
2019-01-07 06:02:13 +0200

    Add files via upload

---
+++ web/lab_5/Client/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {BrokersComponent} from './brokers/brokers.component';
+import {StocksComponent} from './stocks/stocks.component';
+
+const routes: Routes = [
+    { path: 'brokers', component: BrokersComponent },
+    { path: 'stocks', component: StocksComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
